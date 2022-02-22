f34774024e73da8e51cae6b6e6bfa2c886275770
Isidor Kaplan <isidorjkaplan@mail.utoronto.ca>
2022-02-22 15:45:22 -0500

    Working on adding features to processor

--- programs/test.s
+++ programs/test.s
@@ -1,6 +1,8 @@
 START:
     //Pipelined moving r0-r4
     mv r0, #LABEL
+
+
     mv r1, #2
     mv r2, #3
     mv r3, #4
@@ -8,12 +10,6 @@ START:
     //NOP
     mv r0, r0
     mv r0, r0
-    //Storing calculated values to memory
-    st r0,[r0]//=1
-    st r1,[r0]//=2
-    st r2,[r0]//=3
-    st r3,[r0]
-    st r4,[r0] //storing 5
     
     //Pipelined adding
     //add r0, r1 //=3
@@ -23,17 +19,18 @@ START:
     add r4, #5 //=10
     
     //Testing load instruction
-    ld r1, [r0] //=abcd
-    ld r1, [r0] //=abcd
-    ld r1, [r0] //=abcd
+    ld r1, [r0] //r1=0xabcd
+    st r3, [r0] //write 9
+    ld r4, [r0] //r4=9
 
     //NOP
     mv r0, r0
     mv r0, r0
     mv r0, r0
     mv r0, r0
-    mv r0, r0
-    mv r0, r0
+
+    //Testing a store
+
 
     b START
     //NOP
