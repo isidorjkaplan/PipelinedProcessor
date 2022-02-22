7981c6d917e4b709931be2bdc24fbbd4bb097a04
Isidor Kaplan <isidorjkaplan@mail.utoronto.ca>
2022-02-22 11:47:30 -0500

    Trying to resolve compiler errors

--- src/main.sv
+++ src/main.sv
@@ -9,11 +9,11 @@ module tb();
     assign #10 CLOCK = ~CLOCK;
 
     /*Defining the wires to interface with the processor*/
-    logic [WORD_SIZE-1:0] DataIn, InstrIn //input ports for data and instructions
+    logic [15:0] DataIn, InstrIn //input ports for data and instructions
     logic DataWaitreq,
     logic Reset, Clock, Enable, //control signals
-    logic [WORD_SIZE-1:0] DataOut, //Output Data Port for Writes
-    logic [WORD_SIZE-1:0] DataAddr, InstrAddr //Address ports for data and instructions
+    logic [15:0] DataOut, //Output Data Port for Writes
+    logic [15:0] DataAddr, InstrAddr //Address ports for data and instructions
     logic WriteData, ReadData //Instr always assumed read=1
 
     /*Defining the design for testing*/
