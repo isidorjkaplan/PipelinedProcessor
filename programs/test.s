03860717a8aaa6a5fd1161d67555a7fb8d5228b4
Isidor Kaplan <isidorjkaplan@mail.utoronto.ca>
2022-02-22 13:34:09 -0500

    Trying to resolve compiler errors

--- programs/test.s
+++ programs/test.s
@@ -1,42 +1,19 @@
 START:
-    mv r1, #6
-    //Adding a bunch of NOP's
-    mv r0, r0
-    mv r0, r0
-    mv r0, r0
-    mv r0, r0
-    mv r0, r0
-    mv r0, r0
-    //another instruction
-    mv r2, #7
-    //Adding a bunch of NOP's
-    mv r0, r0
-    mv r0, r0
-    mv r0, r0
-    mv r0, r0
-    mv r0, r0
-    mv r0, r0
-    //Another instruction
-    add r1, r2
-    //Adding a bunch of NOP's
-    mv r0, r0
-    mv r0, r0
-    mv r0, r0
-    mv r0, r0
-    mv r0, r0
-    mv r0, r0
-    //store result
-    st r1, [r2] 
-    //write a huge number of zeros
-    mv r0, r0
-    mv r0, r0
-    mv r0, r0
-    mv r0, r0
-    mv r0, r0
-    mv r0, r0
-    mv r0, r0
-    mv r0, r0
-    mv r0, r0
-    mv r0, r0
-    mv r0, r0
-    mv r0, r0
+    //Pipelined moving r0-r4
+    mv r0, #1
+    mv r1, #2
+    mv r2, #3
+    mv r3, #4
+    mv r4, #5
+    //NOP
+    mv r0, #0
+    mv r0, #0, 
+    mv r0, #0
+    mv r0, #0, 
+    mv r0, #0
+    //Pipelined adding
+    add r0, r1 //=3
+    add r1, r2 //=5
+    add r2, r3 //=7
+    add r3, r4 //=9
+    add r4, #5 //=10
