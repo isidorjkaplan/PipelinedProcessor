9994cece86d07154b19a09814c47fb415e5e70b9
Isidor Kaplan <isidorjkaplan@mail.utoronto.ca>
2022-02-22 11:14:36 -0500

    Adding tennative implementation. No hazard detection or stall logic or anything. Have not tested if it even compiles

--- src/processor.sv
+++ src/processor.sv
@@ -1,9 +1,9 @@
 module processor (
-    input [WORD_SIZE-1:0] DataIn, InstIn //input ports for data and instructions
+    input [WORD_SIZE-1:0] DataIn, InstrIn //input ports for data and instructions
     input DataWaitreq,
     input Reset, Clock, Enable, //control signals
     output [WORD_SIZE-1:0] DataOut, //Output Data Port for Writes
-    output [WORD_SIZE-1:0] DataAddr, InstAddr //Address ports for data and instructions
+    output [WORD_SIZE-1:0] DataAddr, InstrAddr //Address ports for data and instructions
     output WriteData, ReadData //Instr always assumed read=1
 );
     /*Defining parameters for readability*/
@@ -17,6 +17,11 @@ module processor (
 
     enum {Fetch=0, Decode=1, Execute=2, Memory=3, Writeback=4} Stages;
     enum {LR=5, SP=6, PC=7} RegNames;
+    //Will change this later
+    enum {Mov, Mvt, Branch, Add, Sub, Load, Store, Logic, Other} Instr;
+    enum {NO_ALU, ADD, SUB, MULT, DIV, LSL, ASL, LSR, ASR, ROR} ALU_OP;
+
+    parameter mv = 3'b000, mvt_b = 3'b001, add = 3'b010, sub = 3'b011, ld = 3'b100, st = 3'b101, and_ = 3'b110, other = 3'b111;
 
     /*Define the registers*/
     logic [WORD_SIZE-1:0] registers[NUM_REGS]; //general purpose register file
@@ -31,22 +36,113 @@ module processor (
         /*Initilization to avoid latch inference*/
         signals = '{default:0};//set all signals to zero to avoid latch
         stage_comb_values = '{default:0, nop:1}; //if nothing else inserted, its a nop
+        ReadData = WriteData = 0;
+        DataOut = 0;
 
         /*Fetch stage*/
-        if (!signals.stall[Fetch]) begin
+        if (signals.stall <= Fetch) begin
             stage_comb_values[Fetch] = '{default:0}; //new empty latched values struct
             signals.write_reg[PC] = 1'b1; //we will write the new pc value
             signals.write_values[PC] = registers[PC] + 1; //by default increment one word
-            signals.stage_comb_values[Fetch].out = InstIn; //latch the instruction value
+            signals.stage_comb_values[Fetch].out = InstrIn; //latch the instruction value
+            InstrAddr = registers[PC]; //show the PC, that is what we want to get on the next cycle. 
         end //else gets a nop by default
 
         /*Decode Stage*/
+        if (signals.stall <= Decode) begin
+            //extract the opcode bits
+            //CASE1:  III M XXX DDDDDDDDD
+            //CASE2:  III M XXX 000000 YYY
+            stage_comb_values[Decode].opcode = stage_regs[Fetch].out[NBITS-1:NBITS-OPCODE_BITS];
+
+            stage_comb_values[Decode].rX = stage_regs[Fetch].out[REG_BITS-1:0];
+            stage_comb_values[Decode].op1 = registers[stage_comb_values[Decode].rX];
+
+            stage_comb_values[Decode].imm = stage_regs[Fetch].out[NBITS-OPCODE_BITS-1];
+
+
+            case (stage_comb_values[Decode].opcode)
+                mv:stage_comb_values[Decode].instr = Mov;
+                mvt_b: begin
+                    if (stage_comb_values[Decode].Imm)
+                        stage_comb_values[Decode].instr = Mvt;
+                    else
+                        stage_comb_values[Decode].instr = Branch;
+                    stage_comb_values[Decode].imm = 1; //the instruction uses immediate either way
+                end
+                add:begin
+                    stage_comb_values[Decode].instr = Add;
+                    stage_comb_values[Decode].alu_op = ADD;
+                end
+                sub:begin 
+                    stage_comb_values[Decode].instr = Sub;
+                    stage_comb_values[Decode].alu_op = SUB;
+                end
+                ld:begin
+                    stage_comb_values[Decode].instr = Load;
+                end
+                st:begin
+                    stage_comb_values[Decode].instr = Store;
+                end
+                and_:begin
+                    stage_comb_values[Decode].instr = Logic;//todo
+                end
+                other:begin
+                    stage_comb_values[Decode].instr = Other;//todo
+                end
+            endcase
+
+            if (stage_comb_values[Decode].imm)
+                //use rest of bits as the operand bits
+                stage_comb_values[Decode].op2 = stage_regs[Fetch].out[NBITS-OPCODE_BITS-REG_BITS-1:0];
+            else begin
+                //decode which registers to grab op2 from and then fetch from that into op2
+                stage_comb_values[Decode].rY = stage_regs[Fetch].out[REG_BITS-1:0];
+                stage_comb_values[Decode].op2 = registers[stage_comb_values[Decode].rY];
+            end
+            //Control signals for reading and writing to memory
+            stage_comb_values[Decode].read = stage_comb_values[Decode].instr == Load;
+            stage_comb_values[Decode].write = stage_comb_values[Decode].instr == Store;
+            //Writeback for all instructions except a store
+            stage_comb_values[Decode].writeback = stage_comb_values[Decode].instr != Store;
+            
+        end
 
         /*Execute Stage*/
+        if (signals.stall >= Execute) begin
+            /*The Execute part of this stage*/
+            case (stage_regs[Decode].alu_op)
+                ADD:stage_comb_values[Execute].out = stage_regs[Decode].op1 + stage_regs[Decode].op2;
+                SUB:stage_comb_values[Execute].out = stage_regs[Decode].op1 - stage_regs[Decode].op2;
+                default://rest TODO
+            endcase
+        end
 
         /*Memory Stage*/
+        if (signals.stall >= Memory && (stage_regs[Execute].read || stage_regs[Execute].write)) begin
+            //LDR OP1, [OP2]
+            DataAddr = stage_regs[Execute].op2;
+            ReadData = stage_regs[Execute].read;
+            WriteData = stage_regs[Execute].write;
+            if (stage_regs[Execute].read) begin
+                ReadData = 1;
+                stage_comb_values[Execute].out = DataIn;
+            end
+            else if (stage_regs[Execute].write) begin
+                WriteData = 1;
+                DataOut = stage_regs[Execute].op1;
+            end
+            if (DataWaitreq)
+                signals.stall = Memory; //stall all earlier stages, we need to wait
+        end
 
         /*Writeback Stage*/
+        if (signals.stall >= Writeback) begin//always true
+            if (stage_regs[Memory].writeback) begin
+                signals.write_reg[stage_regs[Memory].rX] = 1;
+                signals.write_values[stage_regs[Memory].rX] = stage_regs[Memory].out;
+            end
+        end
 
         /*Stall Logic*/
 
@@ -60,12 +156,22 @@ module processor (
     end
 
     always_ff@(posedge Clock) begin
-        /*On the clock write all the combinational output values to the state regs*/
-        stage_regs <= stage_comb_values;
+        if (Reset) begin
+            stage_regs <= 0;
+            registers <= 0;
+        end
+        else begin
+            /*On the clock write all the combinational output values to the state regs*/
+            stage_regs <= stage_comb_values;
 
-        /*Writeback values to their registers*/
-        if (stage_regs[Writeback].writeback) //if the instr currently in writeback wants to writeback
-            registers[stage_regs[Writeback].rX] <= stage_regs[Writeback].out;//write output to regfile
+            /*Writeback values to their registers*/
+            generate
+                for (genvar i = 0; i < NUM_REGS; i++) begin
+                    if (signals.write_reg[i])
+                        registers[i] <= signals.write_values[i];
+                end
+            endgenerate
+        end
     end
 
     /*The latching of the values for each stage*/
@@ -85,6 +191,7 @@ typedef struct packed {
     logic [WORD_SIZE-1:0] op1, op2; //the explicit values of the operands (could get overwridden by forwarding)
 
     logic [WORD_SIZE-1:0] out; //final result, as well as temporary information in intermediate levels
+
     
     logic [OPCODE_BITS-1:0] rX, rY; //the registers sourcing x and y (if applicable)
 
@@ -98,7 +205,10 @@ typedef struct packed {
 
     logic nop; //true/false value if it is a nop, if so all other stuff get ignored
 
-    logic [OPCODE_BITS-1:0] opcode; //the instruction opcode
+    logic [OPCODE_BITS-1:0] opcode;
+    Instr isntr;
+
+    ALU_OP alu_op;
 
     control_signals signals; //decoded control signals for all future levels
 } latched_values;
