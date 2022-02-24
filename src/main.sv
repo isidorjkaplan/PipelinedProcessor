0abf6e6b393e42b3b376d4a4246e99f80a71e55d
Isidor Kaplan <isidorjkaplan@mail.utoronto.ca>
2022-02-23 20:21:12 -0500

    Bug Fixes & Testing - AvalonBus

--- src/main.sv
+++ src/main.sv
@@ -3,6 +3,7 @@
 module tb();
 
     logic CLOCK;
+    assign Clock=CLOCK;
 
     /*Defining the wires to interface with the processor*/
     logic [15:0] DataIn, InstrIn; //input ports for data and instructions
@@ -10,15 +11,14 @@ module tb();
     logic Reset, Clock, Enable; //control signals
     logic [15:0] DataOut; //Output Data Port for Writes
     logic [15:0] DataAddr, InstrAddr; //Address ports for data and instructions
-    logic WriteData, ReadData; //Instr always assumed read=1
+    wire WriteData, ReadData; //Instr always assumed read=1
 
     assign #10 CLOCK = (~CLOCK & ~Reset);
 
     /*Defining the design for testing*/
-    inst_mem InstrMem (InstrAddr[11:0], CLOCK, 16'b0, 1'b0, InstrIn);
+    inst_mem InstrMem (InstrAddr[11:0], Clock, 16'b0, 1'b0, InstrIn);
     //some initilizations
     always_comb begin
-        Clock = CLOCK;
         Enable=1;
     end
 
