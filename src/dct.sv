73cdd5ac0c2cb5e8d869af7847affe20b31d95d7
Isidor Kaplan <isidor.kaplan@utoronto.ca>
2022-03-07 12:40:58 -0500

    Doing lots of work on the actual DCT. Will come back and finish this later

--- src/dct.sv
+++ src/dct.sv
@@ -1,18 +1,71 @@
-module avalon_dct
+module avalon_dct #(
+    parameter MAX_SIZE=(1<<8); //the maximum size of an array that we can DCT
+    parameter NBITS=16;
+)
 (
 	input clk, //Common clock shared by the entire system.
 	input reset, //Common reset shared by the entire system.
 	input [3:0] address, //Address lines from the Avalon bus.
 	input read, //Read request signal from the CPU. Used together with readdata
 	input write,//Wrote request signal from the CPU. Used together with writedata
-	input [15:0] writedata, //Data lines for the CPU to send data to the peripheral. 
+	input [NBITS:0] writedata, //Data lines for the CPU to send data to the peripheral. 
 									//Used together with write.
-	output logic [15:0] out, //Data lines for the peripheral to return data to the CPU. Used
+	output logic [NBITS:0] out, //Data lines for the peripheral to return data to the CPU. Used
 											//together with read.
 	output logic done //Signal to stall the Avalon bus when the peripheral is busy.
 );
+    //IDLE, can accept requests, COS=Calculating cosine terms for this N, DCT=doing DCT
+    parameter ADDR_SETUP = 4'h0;
+    
+    integer size;    //goes up to max size for N, stores 
+    integer term_num;
+    State state;
+    integer M = 5;
+    integer N = NBITS-1-M;
+    logic cos_start;
+    integer PI = $rtoi(3.14159265*(1<<N));
 
+ 
+    logic [NBITS-1:0] signal[MAX_SIZE];
+    logic [NBITS-1:0] result[MAX_SIZE];
 
+    logic [NBITS-1:0] cos_in[MAX_SIZE];
+    logic [NBITS-1:0] cos_out[MAX_SIZE];
+    logic cos_done[MAX_SIZE];
+    genvar i;
+    generate
+        for (i = 0; i < MAX_SIZE; i++) begin
+            cos func(clk, reset, cos_in[i], M, cos_start, cos_done[i], cos_out[i]);
+        end
+    endgenerate
+
+    logic [NBITS-1:0] dct_term;
+    integer K;
+    always_comb begin
+        dct_term = 0;
+        for (integer i = 0; i < size; i++) begin
+            cos_in[i] = (((PI/size)*(2*i + 1)*K) >> 1) % PI; //mod by PI in Qmn format
+            dct_term = dct_term + cos_out[i]*cos_done[i]*signal[i] >> N; //0 until done and then signal*cos
+        end
+    end
+
+    always_ff@(posedge clk, reset) begin
+        if (reset) begin
+            dct_terms <= 0;
+            size <= 0;
+            cos_start <= 0;
+            K <= 0;
+        end
+        else if (write && address == ADDR_SETUP) begin //initilize
+            size <= writedata;
+            term_num <= 0;
+            K <= 0;
+            cos_start <= 0;
+        end
+        else begin
+            
+        end
+    end
 endmodule
 
 
